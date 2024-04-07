bc3af19ff6feed7c9d4aa771c234126753ccd57a
PaulWerner346 <125453705+PaulWerner346@users.noreply.github.com>
2024-04-07 15:29:53 +0200

    Complete the code in the main.js to achieve the goal for the game, without implementing the changes described in the plan for the main.js file.

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
+// Fetch classroom data from classrooms.json
+let classrooms = [];
+fetch('classrooms.json')
+    .then(response => response.json())
+    .then(data => classrooms = data.classrooms);
+
+// Add event listener to the "Start Game" button
 document.getElementById('startGame').addEventListener('click', function() {
-    // Logic to start the game
+    // Ask the user to select a classroom and the number of levels
+    let classroomName = prompt("Enter the classroom name:");
+    let levels = prompt("Enter the number of levels:");
+
+    // Start the game with the selected classroom and levels
+    startGame(classroomName, levels);
 });
 
+// Add event listener to the "Add Classroom" button
 document.getElementById('addClassroom').addEventListener('click', function() {
-    // Logic to add a classroom
+    // Ask the user for the classroom name and the names of the pupils
+    let classroomName = prompt("Enter the classroom name:");
+    let pupilNames = prompt("Enter the names of the pupils (separated by commas):");
+
+    // Add the new classroom to classrooms.json
+    addClassroom(classroomName, pupilNames.split(','));
 });
+
+// Function to start the game
+function startGame(classroomName, levels) {
+    // Logic to start the game
+}
+
+// Function to add a classroom
+function addClassroom(classroomName, pupilNames) {
+    // Logic to add a classroom
+}
